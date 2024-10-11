e20465a508a1434138dfa131aa0b8ddd294c0fd0
Marllon Home <m.w.p.ferreira@outlook.com>
2024-10-11 17:48:44 -0300

    feat: :sparkles: add: delete and update

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,6 +1,15 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+} from '@nestjs/common';
 import { UserService } from './user.service';
 import { CreateUserDto } from 'src/dtos/create-user.dto';
+import { UpdateUserDto } from 'src/dtos/update-user.dto';
 
 @Controller('user')
 export class UserController {
@@ -9,5 +18,28 @@ export class UserController {
   @Post()
   async createUser(@Body() createUserDto: CreateUserDto) {
     return await this.userService.create(createUserDto);
+  }
+
+  @Get()
+  async getUsers() {
+    return await this.userService.getUser();
+  }
+
+  @Get(':id')
+  async getuserById(@Param('id') id: string) {
+    return await this.userService.getUserById(id);
+  }
+
+  @Patch(':id')
+  async patchUser(
+    @Param('id') id: string,
+    @Body() updateUserDto: UpdateUserDto,
+  ) {
+    return await this.userService.updateUser(id, updateUserDto);
+  }
+
+  @Delete(':id')
+  async deleteUser(@Param('id') id: string) {
+    return await this.userService.deleteUser(id);
   }
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,9 +1,10 @@
+import { User } from './../schemas/user.schemas';
 import { Injectable } from '@nestjs/common';
 import { InjectConnection, InjectModel } from '@nestjs/mongoose';
-import { User } from 'src/schemas/user.schemas';
 import { Connection, Model } from 'mongoose';
 import { CreateUserDto } from 'src/dtos/create-user.dto';
 import * as bcrypt from 'bcrypt';
+import { UpdateUserDto } from 'src/dtos/update-user.dto';
 
 @Injectable()
 export class UserService {
@@ -12,18 +13,78 @@ export class UserService {
     @InjectConnection() private connection: Connection,
   ) {}
 
-  async create(createUserDto: CreateUserDto): Promise<User> {
+  async create(
+    createUserDto: CreateUserDto,
+  ): Promise<{ success: boolean; message: string }> {
     const { password, ...userData } = createUserDto;
 
     const salt = await bcrypt.genSalt(10);
     const hashPassword = await bcrypt.hash(password, salt);
 
-    const newUser = new this.userModel({
-      ...userData,
-      hashPassword,
-      salPassword: salt,
-    });
-    const createdUser = new this.userModel(newUser);
-    return createdUser.save();
+    try {
+      const newUser = new this.userModel({
+        ...userData,
+        hashPassword,
+        salPassword: salt,
+      });
+      const createdUser = new this.userModel(newUser);
+      await createdUser.save();
+      return { success: true, message: 'Usuario inserido com sucesso' };
+    } catch (error) {
+      const erroMenssage = error as Error;
+      return { success: false, message: erroMenssage.message };
+    }
+  }
+
+  async getUser(): Promise<User[]> {
+    try {
+      const users = await this.userModel.find();
+      return users;
+    } catch (error) {
+      throw new Error(`Erro ao buscar usuário: ${(error as Error).message}`);
+    }
+  }
+
+  async getUserById(
+    userId: string,
+  ): Promise<
+    { success: boolean; user: User } | { success: boolean; message: string }
+  > {
+    try {
+      const user = await this.userModel.findById(userId).exec();
+      return { success: true, user };
+    } catch (error) {
+      return {
+        success: false,
+        message: `Erro ao buscar usuário: ${(error as Error).message}`,
+      };
+    }
+  }
+
+  async updateUser(
+    id: string,
+    updateUserDto: UpdateUserDto,
+  ): Promise<{ success: boolean; message: string }> {
+    try {
+      await this.userModel.updateOne({ _id: id }, updateUserDto);
+      return { success: true, message: 'Atualizado com sucesso' };
+    } catch (error) {
+      return {
+        success: false,
+        message: `Erro ao buscar usuário: ${(error as Error).message}`,
+      };
+    }
+  }
+
+  async deleteUser(id: string): Promise<{ success: boolean; message: string }> {
+    try {
+      await this.userModel.deleteOne({ _id: id });
+      return { success: true, message: 'Deletado com sucesso' };
+    } catch (error) {
+      return {
+        success: false,
+        message: `Erro ao buscar usuário: ${(error as Error).message}`,
+      };
+    }
   }
 }
